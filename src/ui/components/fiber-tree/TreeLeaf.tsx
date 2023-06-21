0440ef2ab818ece6966a47109aad22d29e8fa871
Patrycja Fogelman <patrycjaf@microsoft.com>
2023-06-21 21:18:38 +0200

    WIP commits POC

--- src/ui/components/fiber-tree/TreeLeaf.tsx
+++ src/ui/components/fiber-tree/TreeLeaf.tsx
@@ -6,38 +6,47 @@ import TreeLeafCaption from "./TreeLeafCaption";
 export interface TreeLeafProps {
   fiberId: number;
   depth?: number;
+  expandable?: boolean;
 }
 
-const TreeLeaf = React.memo(({ fiberId, depth = 0 }: TreeLeafProps) => {
-  const { setFiberElement, groupByParent, showUnmounted, showTimings } =
-    useTreeViewSettingsContext();
-  const fiber = useFiber(fiberId);
-  const children = useFiberChildren(fiberId, groupByParent, showUnmounted);
-  const [expanded, setExpanded] = React.useState(true);
-  const hasChildren = children.length > 0;
+const TreeLeaf = React.memo(
+  ({ fiberId, depth = 0, expandable = true }: TreeLeafProps) => {
+    const { setFiberElement, groupByParent, showUnmounted, showTimings } =
+      useTreeViewSettingsContext();
+    const fiber = useFiber(fiberId);
+    const children = useFiberChildren(fiberId, groupByParent, showUnmounted);
+    const [expanded, setExpanded] = React.useState(true);
+    const hasChildren = children.length > 0;
 
-  if (!fiber) {
-    return null;
+    if (!fiber) {
+      return null;
+    }
+
+    return (
+      <div
+        className={
+          "tree-leaf" +
+          (expandable && fiber.ownerId === 0 ? " render-root" : "")
+        }
+      >
+        <TreeLeafCaption
+          depth={Math.max(depth - 1, 0)}
+          fiber={fiber}
+          expanded={expanded}
+          setExpanded={hasChildren ? setExpanded : undefined}
+          showTimings={showTimings}
+          setFiberElement={setFiberElement}
+        />
+
+        {expandable &&
+          expanded &&
+          children.map(childId => (
+            <TreeLeaf key={childId} fiberId={childId} depth={depth + 1} />
+          ))}
+      </div>
+    );
   }
-
-  return (
-    <div className={"tree-leaf" + (fiber.ownerId === 0 ? " render-root" : "")}>
-      <TreeLeafCaption
-        depth={Math.max(depth - 1, 0)}
-        fiber={fiber}
-        expanded={expanded}
-        setExpanded={hasChildren ? setExpanded : undefined}
-        showTimings={showTimings}
-        setFiberElement={setFiberElement}
-      />
-
-      {expanded &&
-        children.map(childId => (
-          <TreeLeaf key={childId} fiberId={childId} depth={depth + 1} />
-        ))}
-    </div>
-  );
-});
+);
 
 TreeLeaf.displayName = "TreeLeaf";
 
--- src/ui/pages/Commits.tsx
+++ src/ui/pages/Commits.tsx
@@ -1,10 +1,80 @@
 import * as React from "react";
 import { useSelectedId } from "../utils/selection";
 import { useCommits } from "../utils/fiber-maps";
+import { Commit } from "../../common/consumer-types";
+import { Message } from "common-types";
+import TreeLeaf from "../components/fiber-tree/TreeLeaf";
 
 function CommitsPageBadge() {
   const commits = useCommits();
   return <span>{commits.length}</span>;
+}
+
+function CommitsPageCommitEvent(props: { event: Message }) {
+  const { event } = props;
+  return (
+    <tr>
+      <td>{event.id}</td>
+      <td>
+        <TreeLeaf fiberId={event.fiberId} depth={0} expandable={false} />
+      </td>
+      <td colSpan={3}>{event.op}</td>
+    </tr>
+  );
+}
+
+function CommitsPageCommit(props: { commit: Commit }) {
+  const { commit } = props;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const stat = commit.events.reduce((stat, event) => {
+    stat[event.op] = (stat[event.op] || 0) + 1;
+    return stat;
+  }, Object.create(null));
+  console.log(commit);
+
+  const toggleExpand = React.useCallback(
+    () => setExpanded(state => !state),
+    [setExpanded]
+  );
+
+  const trigger = commit.start.event.triggers[0];
+  const expandClass = expanded ? "expanded" : "collapsed";
+  return (
+    <>
+      <tbody
+        id={`commit-${commit.commitId}`}
+        className="commit-data"
+        onClick={toggleExpand}
+      >
+        <tr>
+          <td>
+            <button className={`button-expand ${expandClass}`}></button>
+            {commit.commitId}
+          </td>
+          <td>{stat.mount || ""}</td>
+          <td>{stat.update || ""}</td>
+          <td>{stat.unmount || ""}</td>
+          <td>{`${trigger?.event || ""} ${trigger?.kind || ""}`}</td>
+        </tr>
+      </tbody>
+      {commit.events?.length && (
+        <tbody
+          key={`events-${commit.commitId}`}
+          className={`commit-details ${expandClass}`}
+        >
+          <tr>
+            <th>id</th>
+            <th>fiber</th>
+            <th colSpan={3}>operation</th>
+          </tr>
+          {commit.events.map(e => (
+            <CommitsPageCommitEvent key={e.id} event={e} />
+          ))}
+        </tbody>
+      )}
+    </>
+  );
 }
 
 function CommitsPage() {
@@ -19,30 +89,16 @@ function CommitsPage() {
       <table>
         <thead>
           <tr>
-            <td>#</td>
-            <td>Mounts</td>
-            <td>Updates</td>
-            <td>Unmounts</td>
+            <th>#</th>
+            <th>Mounts</th>
+            <th>Updates</th>
+            <th>Unmounts</th>
+            <th>Trigger</th>
           </tr>
         </thead>
-        <tbody>
-          {commits.map(commit => {
-            const stat = commit.events.reduce((stat, event) => {
-              stat[event.op] = (stat[event.op] || 0) + 1;
-              return stat;
-            }, Object.create(null));
-            console.log(commit);
-
-            return (
-              <tr key={commit.commitId}>
-                <td>{commit.commitId}</td>
-                <td>{stat.mount || ""}</td>
-                <td>{stat.update || ""}</td>
-                <td>{stat.unmount || ""}</td>
-              </tr>
-            );
-          })}
-        </tbody>
+        {commits.map(commit => (
+          <CommitsPageCommit key={commit.commitId} commit={commit} />
+        ))}
       </table>
     </div>
   );
